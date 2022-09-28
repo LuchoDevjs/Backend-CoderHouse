c764bdcc65a4243730a4286d9dc20084407e1be3
Luciano Gonzalez <luchogonzalez080@gmail.com>
2022-09-28 20:07:27 -0300

    Desafio4

---
+++ BackCoder/Desafio4/db/data.js
@@ -0,0 +1,22 @@
+module.exports = [
+  {
+    id: 1,
+    nombre: "Iphone 12",
+    precio: 1000,
+  },
+  {
+    id: 2,
+    nombre: "Ipad",
+    precio: 600,
+  },
+  {
+    id: 3,
+    nombre: "AirPods",
+    precio: 200,
+  },
+  {
+    id: 4,
+    nombre: "Iphone 7",
+    precio: 250,
+  },
+];
