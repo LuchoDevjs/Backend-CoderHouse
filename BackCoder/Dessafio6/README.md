d8d1655dfcf0782c651a3116c38ca7b30ddc5525
Luciano Gonzalez <luchogonzalez080@gmail.com>
2022-10-12 19:18:36 -0300

    desafio6

---
+++ BackCoder/Dessafio6/README.md
@@ -0,0 +1,37 @@
+# WEBSOCKETS
+
+![socket.io](https://i.imgur.com/GpNYLzy.png)
+
+Este es un proyecto en donde se va a utilizar websockets, en este caso **socket.io**
+
+
+## Configuracion
+
+- Para poder iniciar el proyecto se debe clonar o descargar el repositorio
+
+```
+git clone https://github.com/ShannonEchegaray/Desarollo-Backend---Shannon-Echegaray
+```
+
+- A continuacion, ya descargado ejecutar los siguiente comando desde la carpeta "Desarrollo-Backend---Shannon-Echegaray"
+
+```
+git checkout desafio-6
+npm install
+```
+
+- Ya ejecutado, crear un archivo llamado ".env" y dentro escribir lo siguiente
+
+```
+PORT=8080
+NODE_ENV=local
+NODE_URL="http://localhost:8080/"
+```
+
+- Por ultimo ejecutar el comando
+
+```
+npm start
+```
+
+*No se dejara Collection de Postman ya que no se utiliza en todo el desafio*
---
+++ BackCoder/Dessafio6/socket.js
@@ -0,0 +1,50 @@
+const e = require("express");
+const { Server } = require("socket.io");
+const LProductos = require("./db/productos");
+const LMensajes = require("./db/mensajes");
+
+let io;
+
+const initServer = (httpServer) => {
+  io = new Server(httpServer);
+  setEvents(io);
+};
+
+const setEvents = (io) => {
+  io.on("connection", (socketClient) => {
+    console.log("Se ha conectado un nuevo cliente, id: " + socketClient.id);
+
+    if (LProductos.obtenerProductos().length !== 0) {
+      emit("product-history", LProductos.obtenerProductos());
+    }
+
+    LMensajes.obtenerMensajes().then(async (mensajes) => {
+      if (mensajes.length !== 0) {
+        emit("message-history", mensajes);
+      }
+    });
+
+    socketClient.on("disconnection", () => {
+      console.log("Se ha desconectado el cliente con la id " + socketClient.id);
+    });
+
+    socketClient.on("product", (data) => {
+      LProductos.agregarProducto(data);
+      emit("product", data);
+    });
+
+    socketClient.on("message", async (data) => {
+      LMensajes.agregarMensaje(data);
+      emit("message", data);
+    });
+  });
+};
+
+const emit = (action, data) => {
+  io.emit(action, data);
+};
+
+module.exports = {
+  initServer,
+  emit,
+};
